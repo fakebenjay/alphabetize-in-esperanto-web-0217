d4b7aef1b7f9956957ce0cc0f0aa06e475bb68a9
Ben Jay <fakebenjay@gmail.com>
2017-02-01 16:20:02 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,6 +5,7 @@ def alphabetize(array)
   ##.tr sorts with English letters by using them to replace Esperanto letters
   ## ĉ in ĉu vi parolas esperanton becomes d for sorting purposes
   ## all letters are replaced/moved over this way, not just Esperanto ones.
+  ## Tried gsub earlier, but that didn't pass tests
 
   array.sort_by do |word|
     word.tr(esperanto, english)
